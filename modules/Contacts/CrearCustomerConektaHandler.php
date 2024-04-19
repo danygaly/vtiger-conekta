51719550dfc95224b0eb7e4714b05d26fff6ed0d
Daniel Galicia <dgalicia@indracompany.com>
2024-04-19 14:19:19 -0600

    Backend | CreaCostumeConekta | Adicion envio de datos

--- modules/Contacts/CrearCustomerConektaHandler.php
+++ modules/Contacts/CrearCustomerConektaHandler.php
@@ -1,6 +1,7 @@
 <?php 
 
 require_once('include/utils/utils.php');
+require_once('modules/Conekta/Conekta.php');
 
 //Coloca este archivo en vtigercrm/modules/Contacts/
 //Debes registrar el handler ejecutando regisrarEventHandler.php desde la carpeta principal de Vtiger.
@@ -9,22 +10,31 @@ class CrearCustomerConektaHandler extends VTEventHandler
 {
 	public function handleEvent($eventType, $entity)
 	{
+		$api = new ConektaConsumeAPI();
 		$moduleName = $entity->getModuleName();
 		if ($moduleName == 'Contacts' && $eventType == 'vtiger.entity.beforesave') {
 			$idContacto = $entity->getId();
 			$camposContacto = $entity->getData();
-			
-			//Estos son los campos del contacto:
-			echo '<pre>';
-			print_r($camposContacto);
-			echo '</pre>';
-			
-			//$entity->get('nombre_campo') para obtener el valor de un campo.
-			//$entity->set('nombre_campo', 'valor') para establecer el valor de un campo.
-			
+
+			$name = $entity->get('firstname').' '.$entity->get('lastname');
+			$mail = $entity->get('email');
+			$phone = $entity->get('phone');
+
+			$data_costume = array(
+				'name' => $name,
+				'email' => $mail,
+				'phone' => $phone
+			);
+			$data_costume = json_encode($data_costume);
+			$response = json_decode($api->sendDataCostume($data_costume));
+
+			$id_conekta = $response->id;
+
+			$entity->set('otherphone', $id_conekta);
+
 			//Este exit sirve para ver en el navegador el output (echos) de arriba. 
 			//Es necesario quitarlo para que al guardar el contacto en la vista de edición, regrese al usuario a la vista de detalle.
-			exit; 
+			//exit; 
 		}
 	}
 }
